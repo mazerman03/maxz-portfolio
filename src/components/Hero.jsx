1de401474f54c6ba2cbb3233df543197c2aaea12
Max Zermeño Manrique <105751347+mazerman03@users.noreply.github.com>
2025-01-13 13:35:54 -0600

    Added Scroll Button and Fixed Mobile Implementation of 3D Model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,9 +17,25 @@ const Hero = () => {
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>Software Engineer that loves backend <br className="sm:block hidden" />development, data analysis, and Mathematics</p>
         </div>
       </div>
-      {/* TODO: Fix lighting, moving model with left click, and mobile implementation */}
+      {/* TODO: Fix lighting, moving model with left click */}
       <ComputersCanvas />
-    
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center" >
+        <a href="#about">
+          <div className= "w-[32px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div 
+              animate={{
+                y: [0, 24, 0]
+              }}
+              transition={{
+                duration: 1.5, 
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
