246cb2eb03e2448736d63c1f5135f9334b234c16
Max Zermeño Manrique <105751347+mazerman03@users.noreply.github.com>
2025-01-17 14:41:31 -0600

    Fixed to stop user rotation on mobile hero section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -23,6 +23,7 @@ const Computers = ({ isMobile }) => {
             scale={isMobile ? 0.6 : 0.75}
             position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
             rotation={[-0.01, -0.2, -0.1]}
+            
           />
 
 
@@ -72,6 +73,7 @@ const ComputersCanvas = () => {
         maxPolarAngle={Math.PI / 2}
         minPolarAngle={Math.PI / 2}
         enablePan={false}
+        enableRotate= {isMobile ? false : true}
         />
         <Computers isMobile={isMobile}/>
       </Suspense>
